6573825a0f9b49d385656652b24c956c3a85eb72
steven.liatti <steven.liatti@etu.hesge.ch>
2018-04-09 15:20:13 +0200

    Update script.js

--- REST/static/script.js
+++ REST/static/script.js
@@ -9,21 +9,35 @@ var socket;
 
 let canvas = document.getElementById('mycanvas');
 let context = canvas.getContext('2d');
-let imageObj = new Image();
-imageObj.src = 'https://nfc.eracnos.ch/plan';
-imageObj.onload = function() {
-    context.drawImage(this, 0, 0);
-    // drawObject(context, 'chaise', 350, 250);
-};
+
 
 $(document).ready(function() {
     init();
+    loadImage(context, false);
 });
 
 function init() {
     socket = io.connect('https://'+SERVER_IP+':'+SERVER_PORT);
     socket.on(SCAN, socket_io_receive_scan);
     socket.on(SCAN_NOT_FOUND, socket_io_receive_scan_not_found);
+}
+
+function drawObject(context, name, x, y) {
+    context.fillStyle = 'red';
+    context.fillRect(x, y, 10, 10);
+    context.font="18px sans-serif";
+    context.fillText(name, x + 12, y + 9);
+}
+
+function loadImage(context, draw, name='', x=0, y=0) {
+    let imageObj = new Image();
+    imageObj.src = 'https://nfc.eracnos.ch/plan';
+    imageObj.onload = function() {
+        context.drawImage(this, 0, 0);
+        if (draw) {
+            drawObject(context, name, x, y);
+        }
+    };
 }
 
 function socket_io_receive_scan(data) {
@@ -33,7 +47,7 @@ function socket_io_receive_scan(data) {
     $('#location').text(data.location);
     let location = data.location;
     let coord = location.split(',');
-    drawObject(context, data.name, coord[1], coord[2]);
+    loadImage(context, true, data.name, coord[1], coord[2]);
 }
 
 function socket_io_receive_scan_not_found(data) {
@@ -41,11 +55,4 @@ function socket_io_receive_scan_not_found(data) {
     $('#id').text("");
     $('#name').text("");
     $('#location').text("");
-}
-
-function drawObject(context, name, x, y) {
-    context.fillStyle = 'red';
-    context.fillRect(x, y, 10, 10);
-    context.font="18px sans-serif";
-    context.fillText(name, x + 12, y + 9);
 }
